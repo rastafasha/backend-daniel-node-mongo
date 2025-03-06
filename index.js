7319a7ad2ded083621d2f3fb9b040db457eb94b7
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-03-06 13:26:20 -0400

    cors 3

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = require('http').Server(app);
 //cors
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://malcolmcordova.com');
+    res.header('Access-Control-Allow-Origin', 'https://malcolmcordova.com', 'https://backend-daniel-node-mongo.vercel.app/api/uploads/');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
