f35caae3afb9d3042d51d7e1acd6585630683242
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-03-06 13:23:12 -0400

    cors 2

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = require('http').Server(app);
 //cors
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://malcolmcordova.com');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 
 const options = {
     cors: {
-        origin: 'http://localhost:4200, https://malcolmcordova.com/admin-node-articles-shop/',
+        origin: 'https://malcolmcordova.com',
     },
 };
 
